05d37a2dba7543f4aa8540fc065ba844f835eee5
Aaron Grisez <grise100@mail.chapman.edu>
2016-09-20 07:38:48 +0000

    Second Commit, Adding problems 1 and 4 and changes to jupyter

--- fill_arrays_vectorized.py
+++ fill_arrays_vectorized.py
@@ -1,22 +1,36 @@
 #!bin/usr/env python
 
-"""
+import nose as n
+import numpy as np
 
+"""
+Module creates an array containing the evaluation of some h(x), defined here as 1 / sqrt(2 * pi) * e ** (-1 / 2 * x ** 2), at a series of 41 points in the interval [-4,4]
 """
 
 import numpy as np
 
-def fill_array():
-    return np.linspace(-4, 4, 41, endpoint=True)
-
+def fill_array(a, b, n):
+    """
+    Fills array with equally spaced points, taking the beginning point, the end point, and the number of desired coordinates
+    """
+    return np.linspace(a, b, n, endpoint=True)
 
 def h(x):
+    """
+    A definition for the function h(x), returning the value of h evaluated at the point x
+    """
     return 1.0 / np.sqrt(2.0 * np.pi) * np.exp(-1.0 / 2.0 * x ** 2.0)
 
-def main(argv):
-    print [fill_array(), h(fill_array())]
-    return 0
+def main():
+    for x in range(41):
+        print %-5d %-5d % (fill_array(-4, 4, 41)[x], h(fill_array(-4, 4, 41)[x]))
+
+def test_h():
+    """
+    Test for function default h(x) defined above, using the initial point x = 0.0
+    """
+    test = h(0.0)
+    n.tools.assert_almost_equals(test, (1 / np.sqrt(2 * np.pi)), places = 5)
 
 if __name__ == "__main__":
-    import sys
-    main(sys.argv)
+    main()
--- read_2columns.py
+++ read_2columns.py
@@ -1,6 +1,7 @@
 #!bin/usr/env python
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Reads 2 column data file with each column formated as "-----[dat1]-------[dat2]" where dashes are whitespace and [dat1] and [dat2] are the columns, each containing 
@@ -15,18 +16,40 @@ def readDat(f):
     column2 = []
     for line in infile:
         words = line.split()
-        column1.append(words[0])
-        column2.append(words[1])
+        if(isNumber(words[0])):
+            column1.append(words[0])
+        if(isNumber(words[1])):
+            column2.append(words[1])
     col1 = np.array(column1)
     col2 = np.array(column2)
 #   print (col1, col2) Debugging assistance
     return col1, col2
 
+#This function is borrowed from user Daniel Goldberg on Stack Overflow, post at http://stackoverflow.com/q/354038
+def isNumber(s):
+    """
+    This function takes a string as an argument and tries to parse it to float. If it can, it returns true. Else, it returns false. This will filter elements in the .dat file so that only numbers are included in the returned arrays
+    """
+    try:
+        float(s)
+        return True 
+    except ValueError:
+        return False
+
 def main():
     """
     Executes readDat on xy.dat
     """
-    readDat("xy.dat")
+    dat = readDat("xy.dat")
+    plt.plot(dat[0], dat[1], '-r')
+    plt.xlabel('x variable')
+    plt.ylabel('y variable')
+    plt.title('Graphing 2 Column Data')
+    plt.show()
+    print 'Mean y value is ', np.mean(dat[1])
+    print 'Max y value is ', np.amax(dat[1])
+    print 'Minimum y value is ', np.amin(dat[1])
+
 
 def test_readDat():
     """
@@ -37,12 +60,17 @@ def test_readDat():
     success = 0
 
     def a_eq(x,y,tol = 1E-5):
+        x = float(x)
+        y = float(y)
         return (abs(x - y) < tol)
 
-    for i, j in zip(test, case):
+    for (i,j) in zip(test[0], case[0]):
         if a_eq(i,j):
             success += 1
-    assert (success == 3)
+    for (i,j) in zip(test[1], case[1]):
+        if a_eq(i,j):
+            success += 1
+    assert (success == 6)
 
 if __name__ == "__main__":
     main();
